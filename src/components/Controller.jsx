2b55271a1f2314ebf34482ff7416a68017fd29c6
AndrewELe <125837056+AndrewELe@users.noreply.github.com>
2023-11-08 17:11:26 -0500

    working on audio integrations

---
+++ src/components/Controller.jsx
@@ -0,0 +1,21 @@
+import { useReactMediaRecorder } from 'react-media-recorder';
+import React, {useEffect,useState} from 'react';
+
+export default function Controller({}) { //add in user authentication props
+    const [userInput] = useState([])
+
+
+return (
+    
+
+
+
+
+
+    <div>
+        <AudioCapture />
+    </div>
+)
+
+}
+
